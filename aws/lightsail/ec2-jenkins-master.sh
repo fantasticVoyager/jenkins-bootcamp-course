64a5c92ff0400cd2b35b1dafffac90bc11641ef4
fantasticVoyager <31387959+fantasticVoyager@users.noreply.github.com>
2020-06-08 11:44:05 -0400

    update to Jenkins download

--- aws/lightsail/ec2-jenkins-master.sh
+++ aws/lightsail/ec2-jenkins-master.sh
@@ -22,10 +22,10 @@ ln -s maven/bin/mvn /bin/mvn
 
 # install Jenkins
 cd /
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
-yum update -y
-yum install -y jenkins
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+yum upgrade -y
+yum install -y jenkins java-1.8.0-openjdk-devel
 
 systemctl start jenkins
 systemctl enable jenkins
