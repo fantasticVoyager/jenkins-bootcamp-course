981aea3d85bac7c2a05fd787d1c8405d604ae9bd
fantasticVoyager <31387959+fantasticVoyager@users.noreply.github.com>
2020-06-08 12:10:26 -0400

    missed the -y

--- aws/lightsail/ec2-jenkins-master.sh
+++ aws/lightsail/ec2-jenkins-master.sh
@@ -24,8 +24,8 @@ ln -s maven/bin/mvn /bin/mvn
 cd /
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum upgrade
-yum install jenkins java-1.8.0-openjdk-devel
+yum upgrade -y
+yum install -y jenkins java-1.8.0-openjdk-devel
 
 systemctl start jenkins
 systemctl enable jenkins
